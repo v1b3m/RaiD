6ce2f5772c7302fdf3321978552d78b34410127e
Benjamin Mayanja <vibenjamin6@gmail.com>
2021-12-18 18:04:18 +0300

    Add cloudinary

---
+++ raid-fe/utils/cloudinary.ts
@@ -0,0 +1,40 @@
+export const scaleCloudinaryImage = (
+  uri: string,
+  width: number,
+  height: number
+): string => {
+  const parts = uri.split("/upload");
+  return `${parts[0]}/upload/c_scale,h_${height},w_${width}/${parts[1]}`;
+};
+
+export const upload = async (
+  files: File[],
+  cloudName: string,
+  uploadPreset: string
+): Promise<string[]> => {
+  const urls = [];
+  const formData = new FormData();
+  const fileList = Object.keys(files);
+
+  for (const file of fileList) {
+    formData.append("file", files[Number(file)]);
+    formData.append("upload_preset", uploadPreset);
+    formData.append("folder", "raid-ai");
+    // This is using an unauthenticated request
+    // check https://cloudinary.com/documentation/upload_images#uploading_with_a_direct_call_to_the_rest_api
+    const result = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      {
+        method: "post",
+        body: formData,
+      }
+    );
+    const response = (await result.json()) as {
+      url: string;
+      secure_url: string;
+    };
+    urls.push(response.secure_url);
+  }
+
+  return urls;
+};
